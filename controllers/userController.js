57b041164e2189cad3f0f0e76a6715a9a47a022c
spardutti <luisdamian.sp@gmail.com>
2021-03-11 20:36:55 -0300

    sign up complete with validation and password confirmation

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,6 +12,8 @@ exports.signUp = function (req, res, next) {
 
 //SIGN UP FORM POST
 exports.signUpPost = [
+  //VALIDATE FIELDS
+
   body("name")
     .isLength({ min: 1 })
     .escape()
@@ -29,24 +31,43 @@ exports.signUpPost = [
     .isAlphanumeric()
     .withMessage("Usuario debe contener Solo letras y numeros"),
 
-  body("pass", "minimo 6 caracteres").isLength({ min: 6 }),
+  body("pass")
+    .isLength({ min: 6 })
+    .withMessage("contraseña minimo 6 caracteres"),
+
+  body("passconfirm", "las contraseñas deben coincidir")
+    .exists()
+    .custom((value, { req }) => value === req.body.pass),
+
   (req, res, next) => {
+    // create a variable with all the erros of the validation
     const errors = validationResult(req);
 
+    //if errors !empty display the form with the errors
     if (!errors.isEmpty()) {
       res.render("signUp", { errors: errors.array() });
     } else {
-      bcrypt.hash(req.body.pass, 10, function (err, hash) {
-        const user = new User({
-          username: req.body.user,
-          firstName: req.body.name,
-          lastName: req.body.lastname,
-          password: hash,
-        }).save((err) => {
-          if (err) return next(err);
-          //Success
-          res.redirect("/");
-        });
+      //check if the username already exist
+      User.findOne({ username: req.body.user }).exec((err, userExist) => {
+        if (err) return next(err);
+        if (userExist) {
+          res.render("signUp", { error: "El usuario ya existe" });
+        }
+        //encrypt password
+        else {
+          bcrypt.hash(req.body.pass, 10, function (err, hash) {
+            const user = new User({
+              username: req.body.user,
+              firstName: req.body.name,
+              lastName: req.body.lastname,
+              password: hash,
+            }).save((err) => {
+              if (err) return next(err);
+              //Success
+              res.redirect("/");
+            });
+          });
+        }
       });
     }
   },
